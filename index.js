f3cbc514bad9439eb5e78c6d07fa5793e9bf75dc
felipe-u <felipe_uv@outlook.com>
2023-12-02 16:44:10 -0500

    Interface Design And Animation

--- index.js
+++ index.js
@@ -178,6 +178,8 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     backgorund.update()
     shop.update()
+    c.fillStyle = 'rgba(255, 255, 255, 0.05)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
 
@@ -227,7 +229,10 @@ function animate() {
     }) && player.isAttacking && player.frameCurrent === 4) {
         enemy.takeHit()
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+        
+        gsap.to('#enemyHealth', {
+            width: enemy.health + '%'
+        })
     }
 
     // if player misses
@@ -242,7 +247,10 @@ function animate() {
     }) && enemy.isAttacking && enemy.frameCurrent === 2) {
         player.takeHit()
         enemy.isAttacking = false
-        document.querySelector('#playerHealth').style.width = player.health + '%'
+
+        gsap.to('#playerHealth', {
+            width: player.health + '%'
+        })
     }
 
     // if enemy misses
